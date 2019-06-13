7b28e26ea8a0ab50e727a397699efef6dfcdd63b
biadejesus <ra104361@uem.br>
2019-06-13 08:34:36 -0300

    estagios do pipeline adicionados

--- trabalho3.c
+++ trabalho3.c
@@ -3,12 +3,51 @@
 
 #define n 32
 
-int main(){
+/*int tradutor()
+{
 	unsigned int *aux = malloc(sizeof(char) * 8);
 	FILE *instrucoes = fopen("entrada.txt", "r");
 	//n vai ser a entrada c o tamanho
 	unsigned char *memoria = malloc(sizeof(char) * n);
-	for (int i=0; i<n; i+=8){
+	for (int i = 0; i < n; i += 8)
+	{
+		fscanf(instrucoes, "%x", aux);
+		memoria[i] = aux;
+		printf("%X\n", *aux);
+	}
+
+	return 0;
+}
+
+int unidadesFuncionais(){
+	
+}
+
+int emissao(){}
+
+int busca()
+{
+}
+
+int execução()
+{
+} 
+
+int escrita(){}
+
+int reordenacao(){}
+
+int efetivacao(){}
+*/
+
+int main()
+{
+	unsigned int *aux = malloc(sizeof(char) * 8);
+	FILE *instrucoes = fopen("entrada.txt", "r");
+	//n vai ser a entrada c o tamanho
+	unsigned char *memoria = malloc(sizeof(char) * n);
+	for (int i = 0; i < n; i += 8)
+	{
 		fscanf(instrucoes, "%x", aux);
 		memoria[i] = aux;
 		printf("%X\n", *aux);
