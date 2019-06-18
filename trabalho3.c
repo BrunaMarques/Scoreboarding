3a9133d6b45bc77bb4118d859cf4daa336cfac34
Bruna <bruna99stefany@gmail.com>
2019-06-18 09:20:45 -0300

    tentativa de fazer a busca

--- trabalho3.c
+++ trabalho3.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 #define n 32
+struct barreira{
+	unsigned int *reg = malloc(sizeof(char) * 8);
+};
+
+
+//fazer uma struct barreira para cada estágio
 
 /*int tradutor()
 {
@@ -43,18 +49,31 @@ int reordenacao(){}
 int efetivacao(){}
 */
 
+void busca(unsigned char memoria){
+	unsigned int *IR = malloc(sizeof(char) * 8);
+	for (int PC = 0; PC < n; PC += 8){
+		IR = memoria[PC];
+	}
+	barreira.reg = IR;
+}
+
 int main()
 {
 	unsigned int *aux = malloc(sizeof(char) * 8);
 	FILE *instrucoes = fopen("entrada.txt", "r");
 	//n vai ser a entrada c o tamanho
 	unsigned char *memoria = malloc(sizeof(char) * n);
-	for (int i = 0; i < n; i += 8)
-	{
+	for (int i = 0; i < n; i += 8){
 		fscanf(instrucoes, "%x", aux);
 		memoria[i] = aux;
-		printf("%X\n", *aux);
+		printf("AUX: %X\n", *aux);
 	}
+
+	//busca(memoria);
+
+	// for (int i = 0; i < n; i += 8){
+	// 	printf("%X\n", memoria[i]);
+	// }
 
 	return 0;
 }
