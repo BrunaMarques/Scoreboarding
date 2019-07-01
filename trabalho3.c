99ccbfedb99df9dbed50aad9d86dfa0aa15582ae
Bruna <bruna99stefany@gmail.com>
2019-07-01 15:00:20 -0300

    busca feita teoricamente

--- trabalho3.c
+++ trabalho3.c
@@ -3,8 +3,8 @@
 
 int n = 16;
 
-
 char* inicializaMemoria(){
+
 	//procurar tamanho da memoria e por em n // 
 	char *memoria = malloc(sizeof(char) * n);
 	return memoria;
@@ -12,26 +12,56 @@ char* inicializaMemoria(){
 
 void escritaMemoria(int tamanho, char *memoria){
 	FILE *instrucoes = fopen("entrada.txt", "r");
-	unsigned int *instrucao = malloc(sizeof(char) * 8);
+	unsigned int *instrucao = malloc(sizeof(char) * 4);
+
 	for (int contPalavra=0; contPalavra<tamanho; contPalavra+=4){
+
 		fscanf(instrucoes, "%X", instrucao);
+
 		for (int contByte=0; contByte<4; contByte++){
+
 			memoria[contPalavra-contByte+3] = *instrucao&0x0FF;
 			*instrucao = (*instrucao>>8);
+
 		}
 	}
 }
 
-int main()
-{
-	char *memoria = inicializaMemoria();
-	escritaMemoria(n, memoria);
+int leituraMemoria(unsigned int palavra[], char *memoria, int pc){
 
-	for (int i = 0; i<n; i++){
-		printf("Memoria: %X\n", memoria[i]);
+	for (int contByte=0; contByte<4; contByte++){
+		palavra[contByte] = memoria[(4*pc)+contByte];
+
 	}
 
-	//busca(memoria);
+	return ++pc;
+
+}
+
+int main(){
+
+	char *memoria = inicializaMemoria();
+	escritaMemoria(n, memoria);
+	unsigned int palavra[4];
+	int pc = 0;
+
+	for (int i = 0; i<n; i++){
+
+		printf("Memoria POS(%d): %X\n", i, memoria[i]);
+
+	}
+
+	printf("\n\n");
+
+	for (int i = 0; i<4; i++){
+		pc = leituraMemoria(palavra, memoria, pc);
+		printf("Teste %d: %X\n", i+1, palavra[0]);
+		printf("Teste %d: %X\n", i+1, palavra[1]);
+		printf("Teste %d: %X\n", i+1, palavra[2]);
+		printf("Teste %d: %X\n", i+1, palavra[3]);
+		printf("\n\n");
+	
+	}
 
 	return 0;
 }
