4ef468c948436d4cf2ed8a8c7ff20fa826401de4
Bruna <bruna99stefany@gmail.com>
2019-07-15 18:40:32 -0300

    tradutor em andamento...

--- tradutor.c
+++ tradutor.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "tradutor.h"
+
+int readfield(FILE *arq, char* string){
+	int i = 0;
+	char ch = fgetc(arq);
+	while (ch != EOF && ch != ' ' && ch != ','){
+		string[i] = ch;
+		i++;
+		ch = fgetc(arq);
+	}
+	string[i] = '\0';
+	return i;
+}
 
 int main(){
     char texto[100];
     FILE *instrucoes = fopen("entrada.asm", "r");
-    fread(&texto, sizeof(char), 100,instrucoes);
+    readfield()
+    
 }
